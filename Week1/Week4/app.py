035077a6321077f533a6b87a9478c478816f954e
Ming <125284928+MingLin1995@users.noreply.github.com>
2023-07-25 00:31:26 +0800

    Add files via upload

---
+++ Week1/Week4/app.py
@@ -0,0 +1,110 @@
+""" https://ithelp.ithome.com.tw/articles/10258223 關鍵字：網頁模版與資料接口"""
+""" session 參考教學#12"""
+from flask import Flask, render_template, request, redirect, url_for, session
+app = Flask(__name__)
+
+""" 載入背後加密機制 """
+app.secret_key = "密鑰可以是任何的字串，但是不要告訴別人"
+
+
+""" 首頁 """
+
+
+@app.route("/", methods=["GET", "POST"])  # methods 預設GET
+def login():
+    return render_template("login.html")
+
+
+""" 用來儲存註冊的帳號、密碼 """
+users = {
+    "user1": {"username": "user1", "password": "pass1"},
+    "user2": {"username": "user2", "password": "pass2"}
+}
+
+""" 註冊 """
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if username not in users:
+            users[username] = {"username": username, "password": password}
+            # 註冊成功，顯示註冊成功訊息
+            # render_template()第二個參數可以附帶資料內容，HTML透過{{ }}就可以顯示出來
+            return render_template("register.html", message="恭喜，註冊成功！")
+        else:
+            message = "抱歉，帳號已經存在，請嘗試其他帳號。"
+            # 註冊失敗，顯示錯誤訊息
+            return render_template("register.html", message="抱歉，帳號已經存在，請嘗試其他帳號。")
+
+    return render_template("register.html", message=None)  # 第一次請求時，不顯示任何訊息
+
+
+""" 登入 """
+
+
+@app.route("/signin", methods=["POST"])
+def signin():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        # 檢查是否有輸入帳號密碼
+        if not username or not password:
+            # 網頁重新導向，並且添加要求字串
+            return redirect(url_for("error", message="帳號或密碼不得為空白"))
+
+        # 進行登入驗證
+        if username in users and users[username]["password"] == password:
+            # 將 "logged_in" 狀態設置為 True 表示登入成功
+            session["logged_in"] = True
+            return redirect(url_for("member"))  # 登入成功，轉跳到/member頁面
+        else:
+            # 登入失敗，轉跳到/error頁面
+            return redirect(url_for("error", message="帳號或密碼輸入錯誤"))
+
+
+""" 成功 """
+
+
+@app.route("/member")
+def member():
+    # 檢查 "logged_in" 狀態如果為 False，直接轉跳到首頁
+    if not session.get("logged_in", False):
+        return redirect(url_for("login"))
+    return render_template("member.html")
+
+
+""" 登出 """
+
+
+@app.route("/signout", methods=["GET"])
+def signout():
+    # 將 "logged_in" 狀態設置為 False 表示登出
+    session["logged_in"] = False
+    return redirect(url_for("login"))
+
+
+""" 失敗 """
+
+
+@app.route("/error")
+def error():
+    # 從URL查詢字串中，獲取message
+    message = request.args.get("message")
+    return render_template("error.html", message=message)
+
+
+""" 計算 """
+
+
+@app.route("/square/<int:number>", methods=["GET"])
+# 註冊路由除了固定的導向位址，URL 也可以成為函式接收的參數
+def square(number):
+    squared_number = number ** 2
+    return render_template("square.html", number=number, squared_number=squared_number)
+
+
+""" 啟動 """
+app.run(port=3000)
---
+++ Week1/Week4/static/login.js
@@ -0,0 +1,24 @@
+function checkForm() {
+  var agreementCheckbox = document.getElementById("agreement");
+  if (agreementCheckbox.checked) {
+    /* 已經勾選同意條款，返回 true，允許表單提交 */
+    return true;
+  } else {
+    /* 沒有勾選同意條款，彈出警告視窗，並返回 false，阻止表單提交 */
+    alert("請勾選同意條款");
+    return false;
+  }
+}
+
+function calculate() {
+  /* 獲取使用者輸入的數字 */
+  const number = document.getElementById("numberInput").value;
+  /* 檢查是否為正整數 */
+  if (isNaN(number) || Number(number) <= 0) {
+    alert("請輸入正整數");
+  } else {
+    /* https://ithelp.ithome.com.tw/articles/10190062 */
+    /* 重定向到Squared Number Page */
+    window.location.href = "/square/" + number;
+  }
+}
